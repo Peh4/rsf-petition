c1c4dd3f39d39f61be3f4575dd6f327c06c081c6
Peh4 <peh4@MacBook-Air-de-Peha.local>
2015-01-15 22:05:38 +0100

    first-commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/peh4/git/rsf-petition/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ rsf_petition/rsf_petition/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for rsf_petition project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rsf_petition.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
